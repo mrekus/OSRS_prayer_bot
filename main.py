3b1bbb87d0f2e9081ea677cf12a4df3612c5144a
Marius R <marius.rekus@gmail.com>
2023-03-26 20:32:00 +0300

    Beveik veikiantis su 1 press

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import win32api
 import pyautogui
+import keyboard
 import time
 from random import uniform, randint
 
 
 def main():
-    x = 1
-    while x < 25:
-        click_inventory()
-        click_altar()
-        time.sleep(round(uniform(0.08, 0.12), 2))
-        x += 1
+    while True:
+        if keyboard.read_key() == "1":
+            for _ in range(0, 26):
+                click_inventory()
+                click_altar()
+                time.sleep(round(uniform(0.2, 0.3), 2))
+                main()
 
 
-def click_inventory(x=randint(1130, 1142), y=randint(347, 362)):
+def click_inventory(x=randint(1569, 1596), y=randint(454, 477)):
     win32api.SetCursorPos((x, y))
     time.sleep(round(uniform(0.08, 0.12), 2))
     pyautogui.click(clicks=1, interval=(round(uniform(0.02, 0.05), 2)))
     time.sleep(round(uniform(0.07, 0.10), 2))
 
 
-def click_altar(x=randint(1128, 1146), y=randint(387, 402)):
+def click_altar(x=randint(1006, 1152), y=randint(256, 341)):
     win32api.SetCursorPos((x, y))
     time.sleep(round(uniform(0.1, 0.2), 2))
     pyautogui.click(clicks=1, interval=(round(uniform(0.02, 0.05), 2)))
